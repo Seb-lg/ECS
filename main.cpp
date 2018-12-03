152e893153a9259ffcc0f7bfdc17526d09645329
Seb <sebastien.le-gall@epitech.eu>
2018-12-03 15:41:02 +0100

    push before full reset

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include "ecs/Ecs.hpp"
-#include "test/ComponentTest.hpp"
+
+struct test {
+        test() {}
+        test(bool dis, std::string str) {
+            this->display = dis;
+            this->str = str;
+        }
+        bool display;
+        std::string str;
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    ID id = ecs::entity::Entity::getId();
+    ID id;
 
+    for (int i = 0; i < 150000 ; i++) {
+        id = ecs::entity::Entity::getId();
+        ecs::Ecs::addComponent<test>(id, i % 2 == 0, "AGA" + std::to_string(i));
+    }
+    auto list = ecs::Ecs::getComponentMap<test>();
 
-    ecs::Ecs::get().addComponent<int>(id, 9);
-    ecs::Ecs::get().addComponent<CpntTest>(id, std::string("JE SUIS UN TEST"), 9);
-    //oui.addComponent<std::string>(id, std::string("JE SUIS UN TEST"));
+    for (auto o : list) {
+        for (auto i : list) {
+        	if (i.second.str != std::string("AGA12860"))
+        		std::cout << i.second.str << std::endl;
+        }
+	    std::cout << o.second.str << std::endl;
+    }
 
-    auto listss = ecs::Ecs::get().getComponentList<int>();
-
-    for (auto o : listss)
-    	std::cout << o <<std::endl;
-
-    ecs::Ecs::get().get<int>()[id]++;
-
-    listss = ecs::Ecs::get().getComponentList<int>();
-
-    for (auto o : listss)
-    	std::cout << o <<std::endl;
 
     return 0;
 }
